5ece10acd1e86aeae5b6c30f641e9608487c97b9
swatts7 <36054334+swatts7@users.noreply.github.com>
2023-11-14 11:47:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,51 @@
-
-import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import csv
 
 def scrape_data(url):
     try:
         response = requests.get(url)
+        response.raise_for_status()
+
         soup = BeautifulSoup(response.content, 'html.parser')
-        
-        # Extracting data using the specified classes
-        data = {}
-        data['URL'] = url
-        data['Annual Fee'] = soup.find("span", class_="cmp-banner-product-right-aligned__feature--text--number", string="Annual Fee").find_next_sibling("span").text.strip()
-        data['Purchase Interest Rate'] = soup.find("span", class_="cmp-banner-product-right-aligned__feature--text--number", string="Interest: Purchases").find_next_sibling("span").text.strip()
-        data['Cash Advance Interest Rate'] = soup.find("span", class_="cmp-banner-product-right-aligned__feature--text--number", string="Interest: Cash Advance").find_next_sibling("span").text.strip()
-        data['Additional Cardholder Fee'] = soup.find("span", class_="cmp-banner-product-right-aligned__feature--text--number", string="Additional Cardholder").find_next_sibling("span").text.strip()
+        data = []
+
+        # Extracting required information
+        labels = soup.find_all('span', class_='cmp-banner-product-right-aligned__feature--subtext cmp-text')
+        values = soup.find_all('span', class_='cmp-banner-product-right-aligned__feature--text--number')
+
+        for label, value in zip(labels, values):
+            if 'Annual Fee' in label.text:
+                data.append(value.text.strip())
+            elif 'Interest: Purchases' in label.text:
+                data.append(value.text.strip())
+            elif 'Interest: Cash Advance' in label.text:
+                data.append(value.text.strip())
+            elif 'Additional Cardholder' in label.text:
+                data.append(value.text.strip())
+
         return data
-    except Exception as e:
-        return {"URL": url, "Error": str(e)}
+    except requests.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"An error occurred: {err}")
 
-def main():
-    st.title("Credit Card Info Extractor")
-    
-    # Text area for input URLs
-    urls = st.text_area("Enter the URLs (one per line)")
-    if urls:
-        url_list = urls.split("\n")
-        
-        # Button to start scraping
-        if st.button("Scrape Data"):
-            results = [scrape_data(url) for url in url_list]
-            df = pd.DataFrame(results)
-            st.write(df)
-            
-            # Download link for CSV
-            csv = df.to_csv(index=False)
-            st.download_button(label="Download data as CSV",
-                               data=csv,
-                               file_name='credit_card_data.csv',
-                               mime='text/csv')
+def main(urls):
+    # CSV file headers
+    headers = ['URL', 'Annual Fee', 'Purchase Interest Rate', 'Cash Advance Interest Rate', 'Additional Cardholder Fee']
 
-if __name__ == "__main__":
-    main()
+    with open('credit_card_data.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+
+        for url in urls:
+            data = scrape_data(url)
+            if data:
+                writer.writerow([url] + data)
+
+# List of URLs to scrape
+urls = [
+    # Add your URLs here
+]
+
+main(urls)
